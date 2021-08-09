34d019fcd7d4a9fe8ef5b0d3aad6636b6a03d238
Mary1812 <mariyapiligrim3@gmail.com>
2021-08-09 17:02:20 +0300

    Add all tasks

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// const parsCol = document.getElementsByTagName('p');
+
+// const par = document.getElementById('unique');
+
+// const testClass = document.getElementsByClassName('test');
+
+// // * -----------------------------------------------------
+// const coolerPar = document.querySelector('#unique > span');
+
+// const coolerPars = document.querySelectorAll('p');
+
+// const arr = [...coolerPars];
+
+// найти h1, img и span в параграфе
+
+
+// const par = document.getElementById('ph');
+// const img = document.getElementById('img63');
+// const [span] = document.getElementsByClassName('span123');
+
+const h1 = document.querySelector('.main > h1');
+
+const img = document.querySelector('.main > img');
+const span = document.querySelector('span');
