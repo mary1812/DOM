da9cbd36472a3361bbebe90ca6239aad11e45494
Mary1812 <mariyapiligrim3@gmail.com>
2021-08-12 16:56:21 +0300

    Add task divStyleBackground

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,69 @@
 // const img = document.getElementById('img63');
 // const [span] = document.getElementsByClassName('span123');
 
-const h1 = document.querySelector('.main > h1');
+// const h1 = document.querySelector('.main > h1');
 
-const img = document.querySelector('.main > img');
-const span = document.querySelector('span');
+// const img = document.querySelector('.main > img');
+// const span = document.querySelector('span');
+
+
+// const img = document.querySelector('.slider-img');
+
+// const [prevBtn, nextBtn] = document.querySelectorAll('.slider-container' > button);
+
+// const sliderImmages = []
+
+// function makeCounter() {
+//   let counter = 0;
+
+//   return function increment() {
+//     counter = counter += 1;
+//     return counter;
+//   }
+// }
+
+// const increment1 = makeCounter();
+
+// function makeAdder(number) {
+//   let adder = makeAdder(number);
+//   return function getReturn() {
+//      return adder 
+//    }
+// }
+
+// const adder = makeAdder(number)
+
+
+
+// function makeAdder(number) {
+//   let adder = makeAdder(number);
+//   return function getReturn() {
+//     return adder 
+//  }
+// }
+
+// function makeAdder(n) {
+//   let number = n;
+//   return function (m) {
+//     number = number + m;
+//     return number;
+//   }
+// }
+
+
+// const oneLineAdder = (state) => (number) => state += number;
+
+// const adder = makeAdder(20);
+const div = document.querySelector('#root')
+const btns = document.querySelectorAll('#root > button');
+
+function listener(e) {
+  const { target: btn } = e;
+  const { dataset: { backgroundColor }} = btn;
+
+  div.style.backgroundColor = backgroundColor;
+}
+
+for (const btn of btns) {
+  btn.addEventListener('click', listener)
+}
